a80145ea33b414cf4fedac961eecf0365a7d2a33
SupriyaShekar123 <s.supriya82@gmail.com>
2020-07-03 12:31:22 +0200

    added constants file

--- seeders/20200629130607-users.js
+++ seeders/20200629130607-users.js
@@ -1,4 +1,6 @@
 "use strict";
+const bcrypt = require("bcrypt");
+const { SALT_ROUNDS } = require("../config/constants");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -8,40 +10,40 @@ module.exports = {
         {
           name: "John",
 
-          email: "John@1234",
-          password: "test1234",
+          email: "John@1234.com",
+          password: bcrypt.hashSync("test1234", SALT_ROUNDS),
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           name: "Mark",
 
-          email: "Mark@1234",
-          password: "Mark1234",
+          email: "Mark@1234.com",
+          password: bcrypt.hashSync("Mark1234", SALT_ROUNDS),
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           name: "Smita",
 
-          email: "Smita@1234",
-          password: "Smita1234",
+          email: "Smita@1234.com",
+          password: bcrypt.hashSync("Smita1234", SALT_ROUNDS),
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           name: "Rose",
 
-          email: "Rose@1234",
-          password: "Rose1234",
+          email: "Rose@1234.com",
+          password: bcrypt.hashSync("Rose1234", SALT_ROUNDS),
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           name: "Thomas",
 
-          email: "Thomas@1234",
-          password: "Tom1234",
+          email: "Thomas@1234.com",
+          password: bcrypt.hashSync("Tom1234", SALT_ROUNDS),
           createdAt: new Date(),
           updatedAt: new Date(),
         },
